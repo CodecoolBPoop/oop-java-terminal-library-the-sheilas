234dc849681fef71699699b0b8e49e9427a5c33a
myrelin <anna.iklody@gmail.com>
2018-11-14 13:50:24 +0100

    feature added: os clear, output coloring

--- src/com/codecool/termlib/Terminal.java
+++ src/com/codecool/termlib/Terminal.java
@@ -63,9 +63,18 @@ public class Terminal {
      *
      * @param color The color to set.
      */
-    public void setColor(Color color) {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    
+    public static String setColor(String color, String string) {
+        
+        return color + string + RESET;
     }
-
+    
     /**
      * Set the background printing color.
      *
